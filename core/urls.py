548da15b78a0e6703dd1e4a2ae4670eaa9db8a41
sengxingstx.dev <sengxingstx.dev@gmail.com>
2023-05-17 15:26:55 +0700

    simple django templates

--- core/urls.py
+++ core/urls.py
@@ -5,9 +5,6 @@ from .import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('search/<str:keyword>/<int:page>/', views.search, name='search'),
-    path('redirect/<path:url>/', views.redirect, name='redirect'),
-    path('date/<int:day>-<int:month>-<int:year>/', views.date, name='date'),
-    path('article/<int:id>/<slug:title>/', views.article, name='article'),
-    path('map/', views.map, name='map'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
 ]
--- core/views.py
+++ core/views.py
@@ -3,32 +3,13 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('<h1>Hello, World</h1>')
+    return render(request, 'core/home.html')
 
 
-def search(request, keyword, page):
-    return HttpResponse(f'<h1>Search for: {keyword}, Page: {page}</h1>')
+def about(request):
+    return render(request, 'core/about.html')
 
 
-def date(request, day, month, year):
-    return HttpResponse(f'<h1>Date: {day} - {month} - {year}</h1>')
+def contact(request):
+    return render(request, 'core/contact.html')
 
-
-def redirect(request, url):
-    return HttpResponse(f'<a href="{url}" target="_blank">Click here to redirect</a>')
-
-
-def article(request, id, title):
-    return HttpResponse(f'<h1>Article ID: {id} Title: {title}')
-
-
-# http://localhost:8000/map/?type=satellite&lat=13.34&log=12.34&zoom=1
-def map(request):
-    print('request:', request)
-
-    type = request.GET.get('type')
-    lat = request.GET.get('lat')
-    log = request.GET.get('log')
-    zoom = request.GET.get('zoom')
-
-    return HttpResponse(f'<h1>Type: {type}, Lat: {lat}, Log: {log} and Zoom: {zoom}.</h1>')
